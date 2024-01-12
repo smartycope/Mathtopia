eb1e72c027a895963c8b56e270dba16d6b4e8456
Copeland Carter <smartycope@gmail.com>
2024-01-11 20:17:42 -0700

    Added some physics equations

--- pages/Frequent_Expressions.py
+++ pages/Frequent_Expressions.py
@@ -1,99 +1,115 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
-def button(eq):
-    if st.button(eq):
-        st.session_state['_expr'] = eq
+def eq(expr):
+    if st.button(expr):
+        st.session_state['_expr'] = expr
         switch_page('main ')
 
 with st.expander('Volume and Area'):
     "Volume of a Cylinder"
-    button("π * height * radius**2")
+    eq("π * height * radius**2")
 
     "Volume of a Cone"
-    button("(1/3) * π * height * radius**2")
+    eq("(1/3) * π * height * radius**2")
 
     "Volume of a Prism"
-    button("base * height")
+    eq("base * height")
 
     "Volume of a Sphere"
-    button("(4/3) * π * radius**3")
+    eq("(4/3) * π * radius**3")
 
     "Circumfrence of a Circle"
-    button("2 * π * radius")
+    eq("2 * π * radius")
 
     "Area of a Circle"
-    button("π * radius**2")
+    eq("π * radius**2")
 
     "Area of a Traingle"
-    button("(base * height) / 2")
+    eq("(base * height) / 2")
 
     "Surface Area of a Sphere"
-    button("4 * radius**2")
+    eq("4 * radius**2")
 
     "Lateral Surface Area of a Cone"
-    button("π * radius * slantHeight")
+    eq("π * radius * slantHeight")
 
     "Area Between 2 Functions"
-    button("Integral(f(x) - g(x), (x, lowerbound, upperbound))")
+    eq("Integral(f(x) - g(x), (x, lowerbound, upperbound))")
 
 with st.expander('Math Formulae'):
     "Definition of a Derivative"
-    button("Limit((f(x+h) - f(x)) / h, h, 0)")
+    eq("Limit((f(x+h) - f(x)) / h, h, 0)")
 
     "Pythagorean Theorem"
-    button("sqrt(a**2 + b**2) = c")
+    eq("sqrt(a**2 + b**2) = c")
 
     "Slope Function"
-    button("a*x + b = y")
+    eq("a*x + b = y")
 
     "Function of a Circle"
-    button("x**2 + y**2 = radius**2")
+    eq("x**2 + y**2 = radius**2")
 
 with st.expander('Electronics'):
     "Kirchoff's current law"
     st.caption("The sum of the currents entering a node or closed loop is 0")
-    button("Sum(nthCurrentEnteringNode, (n, 1, numBrachesConnectedToNode)) = 0")
+    eq("Sum(nthCurrentEnteringNode, (n, 1, numBrachesConnectedToNode)) = 0")
 
     # "Kirchoff's voltage law"
     # st.caption("The sum of the voltages around a closed loop is 0")
-    # button("Sum(high=numBrachesInLoop, low='n=1', nthVoltage) == 0")
+    # eq("Sum(high=numBrachesInLoop, low='n=1', nthVoltage) == 0")
 
     "Resistance of a Wire"
-    button("resistivity * (length / crossSectionalArea) = Resistance ")
+    eq("resistivity * (length / crossSectionalArea) = Resistance ")
 
     "Charge or Something"
-    button("Integral(time, time_0, current(time) = Charge(time)")
+    eq("Integral(time, time_0, current(time) = Charge(time)")
 
+"# Physics"
 with st.expander('Physics'):
+    "Newton's Law"
+    eq('mass * acceleration = force')
+
+    "Newton's Law Friction"
+    eq('coefficient_friction * newtons = force')
+
+    "Drag Equation"
+    eq('-b * velocity = draw_force')
+
+    "Thrust"
+    eq('mass_flow_rate * exhaust_velocity = force')
+
+    "Average Velocity"
+    eq('displacement / time = velocity')
+
     "Get Velocity with a Constant Acceleration"
-    button("velocity_0 + (constantAcceleration * time) = velocity")
+    eq("velocity_0 + (constantAcceleration * time) = velocity")
 
     "x Position given a Constant Acceleration"
-    button("x_0 + (1/2) * (velocity - velocity_0) * time = x")
+    eq("x_0 + (1/2) * (velocity - velocity_0) * time = x")
 
     "x Position given a Constant Acceleration 2"
-    button("x_0 + (velocity_0 * time) + ((1/2) * constantAcceleration * (time**2)) = x")
+    eq("x_0 + (velocity_0 * time) + ((1/2) * constantAcceleration * (time**2)) = x")
 
     "Velocity**2 given a Constant Acceleration"
-    button("(velocity_0**2) + 2 * constantAcceleration * (x - x_0) = velocity**2")
+    eq("(velocity_0**2) + 2 * constantAcceleration * (x - x_0) = velocity**2")
 
     "Power"
-    button("torque * speed = power ")
+    eq("torque * speed = power ")
 
 with st.expander('Motion'):
     "Vertical Velocity"
-    button("velocity_y0 - gravity * time = velocity_y ")
+    eq("velocity_y0 - gravity * time = velocity_y ")
 
     "Vertical Velocity at time"
-    button("y_0 + velocity_y0 * time - (1/2)*gravity*(time**2) = y")
+    eq("y_0 + velocity_y0 * time - (1/2)*gravity*(time**2) = y")
 
     "Hybird Veritical Velocity"
-    button("sqrt((velocity_y0**2) - 2 * gravity * (y-y_0)) = velocity_y")
+    eq("sqrt((velocity_y0**2) - 2 * gravity * (y-y_0)) = velocity_y")
 
     "Average Angular Speed (𝜔)"
     st.caption("angle is in radians")
-    button(" 𝚫angle/𝚫time = avgAngSpeed")
+    eq(" 𝚫angle/𝚫time = avgAngSpeed")
 
     "Angular Acceleration (𝛼)"
-    button("𝚫avgAngSpeed/𝚫time = angAccel")
+    eq("𝚫avgAngSpeed/𝚫time = angAccel")
